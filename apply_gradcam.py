4b56b2d44eaff66e712c65e9ba28a02cb93ecb78
kazzastic <rkazim79@gmail.com>
2020-03-29 02:18:45 +0500

    Here we go

---
+++ apply_gradcam.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 02:16:54 2020
+
+@author: kazzastic
+"""
+
---
+++ scripts/gradcam.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 02:16:41 2020
+
+@author: kazzastic
+"""
+
